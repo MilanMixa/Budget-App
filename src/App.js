6a40c37b38da19f29fee0daf2482db0614a6cc15
MilanMixa <stankovicmilan990@gmail.com>
2022-02-05 21:50:30 +0100

    budget app finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+//components
+
+import Header from "./components/Header";
+import { ExpensesList } from "./components/ExpensesList";
+import { IncomeAndExpenses } from "./components/IncomeAndExpenses";
+import { IncomeList } from "./components/IncomeList";
+import { InputFields } from "./components/InputFields";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <IncomeAndExpenses />
+      <InputFields />
+      <div className="IncomeExpenseList">
+        <IncomeList />
+        <ExpensesList />
+      </div>
     </div>
   );
 }
